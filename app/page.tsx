37a8d1279279680c4a5cbd488e8bb6ffaf18da22
Manan Singh <mananmessi491@gmail.com>
2025-06-12 14:49:26 +0530

    removed learning section for test

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="gap-20 flex flex-col items-center justify-center text-zinc-200 m-10 sm:mx-15 lg:mx-20">
       <Intro />
       <Skills />
-      <Learning />
+      {/* <Learning /> */}
       <div>
         <Link href="/projects" className="flex">
           See some of my projects <ArrowRight />
